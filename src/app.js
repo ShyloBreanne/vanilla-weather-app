c95860d6f00b264962efd6a76de59c6dc6e3d548
ShyloBreanne <122849178+ShyloBreanne@users.noreply.github.com>
2023-03-11 12:20:37 -0800

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -20,16 +20,21 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+    
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.time * 1000);
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+    iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "0f394coc58tc83ab43c50095f3bd3ad9";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Lisbon}&key=${apiKey}&units=metric`;
+let city = "Lisbon";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
