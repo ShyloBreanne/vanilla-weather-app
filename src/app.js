f88f9300ef220534319dfe10629121b60e718bcd
ShyloBreanne <122849178+ShyloBreanne@users.noreply.github.com>
2023-04-03 10:54:25 -0700

    Added dark theme image

--- src/app.js
+++ src/app.js
@@ -98,3 +98,17 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Portland");
+
+function changeTheme() {
+        let body = document.querySelector("body");
+
+        //  body.classList.toggle("dark");
+
+        if (body.classList.contains("dark")) {
+          body.classList.remove("dark");
+        } else {
+          body.classList.add("dark");
+        }
+      }
+      let themeButton = document.querySelector(".theme-button");
+      themeButton.addEventListener("click", changeTheme);
